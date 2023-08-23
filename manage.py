fb7c4f42e238003784f9ccaac990ec81731c8b7a
Sara Mentzer <sara_mentzer@hotmail.com>
2023-08-23 19:44:09 +0000

    Added static and css

--- manage.py
+++ manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weseeu_services.settings')
+    jls_extract_var = 'DJANGO_SETTINGS_MODULE'
+    os.environ.setdefault(jls_extract_var, 'weseeu_services.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- weseeu_services/settings.py
+++ weseeu_services/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-saram88-weseeuservices-yxzz3c49wtb.ws-eu104.gitpod.io']
 
 
 # Application definition
@@ -42,7 +42,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'boutique_ado.urls'
+ROOT_URLCONF = 'weseeu_services.urls'
 
 TEMPLATES = [
     {
@@ -83,7 +83,7 @@ ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
 
-WSGI_APPLICATION = 'boutique_ado.wsgi.application'
+WSGI_APPLICATION = 'weseeu_services.wsgi.application'
 
 
 # Database
